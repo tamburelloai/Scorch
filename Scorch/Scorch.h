d1576dbf62e2c18063fd9e092d5b273cfbdf29a0
Michael Tamburello <michaeljtamburello@gmail.com>
2024-06-06 20:05:48 -0500

    Initial Commit

---
+++ Scorch/Scorch.h
@@ -0,0 +1,18 @@
+//
+//  Scorch.h
+//  Scorch
+//
+//  Created by Michael Tamburello on 6/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Scorch.
+FOUNDATION_EXPORT double ScorchVersionNumber;
+
+//! Project version string for Scorch.
+FOUNDATION_EXPORT const unsigned char ScorchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scorch/PublicHeader.h>
+
+
