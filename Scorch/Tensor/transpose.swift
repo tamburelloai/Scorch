7014422da23d812b2937b12dbeaaf247f4768a1c
Michael Tamburello <michaeljtamburello@gmail.com>
2024-06-08 19:53:52 -0500

    added transpose

---
+++ Scorch/Tensor/transpose.swift
@@ -0,0 +1,8 @@
+//
+//  transpose.swift
+//  Scorch
+//
+//  Created by Michael Tamburello on 6/8/24.
+//
+
+import Foundation
---
+++ ScorchTests/TensorTests/transposeTests.swift
@@ -0,0 +1,8 @@
+//
+//  transposeTests.swift
+//  ScorchTests
+//
+//  Created by Michael Tamburello on 6/8/24.
+//
+
+import Foundation
