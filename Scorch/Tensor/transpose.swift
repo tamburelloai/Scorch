07463c1453d70a7b2e852921255e6cfaf160e917
Michael Tamburello <michaeljtamburello@gmail.com>
2024-06-08 19:54:17 -0500

    added transpose

--- Scorch/Tensor/transpose.swift
+++ Scorch/Tensor/transpose.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+extension Tensor {
+  mutating func transpose(_ dim0: Int, _ dim1: Int) {
+    assert(dim0 < shape.count && dim1 < shape.count, "Dimension indices are out of bounds.")
+    shape.swapAt(dim0, dim1)   // Swap the shape dimensions
+    strides.swapAt(dim0, dim1) // Swap the corresponding strides
+  }
+  
+  func transpose(dim0: Int, dim1: Int) -> Tensor {
+    assert(dim0 < shape.count && dim1 < shape.count, "Dimension indices are out of bounds.")
+    var newShape = self.shape.map {$0}   // Swap the shape dimensions
+    var newStrides = self.strides.map {$0}   // Swap the shape dimensions
+    newShape.swapAt(dim0, dim1)
+    newStrides.swapAt(dim0, dim1)
+    return Tensor(data: self.data, shape: newShape, strides: newStrides)
+  }
+}
